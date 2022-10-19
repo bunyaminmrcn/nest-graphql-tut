845fa6ff8956bd6cc9100a6e572ad734f73f8bea
bunyaminmrcn <bunyaminmrcn@gmail.com>
2022-10-19 21:54:00 +0300

    Run 'nest g resource owners' command

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PetsModule } from './pets/pets.module';
+import { OwnersModule } from './owners/owners.module';
 
 @Module({
   imports: [
@@ -21,7 +22,9 @@ import { PetsModule } from './pets/pets.module';
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
     
-    PetsModule],
+    PetsModule,
+    
+    OwnersModule],
   controllers: [AppController],
   providers: [AppService],
 })
