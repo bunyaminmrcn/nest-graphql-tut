73f6fda5f5f41d956db8d6c2edd386ae0eae8200
bunyaminmrcn <bunyaminmrcn@gmail.com>
2022-10-19 21:13:52 +0300

    typeorm added to project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -8,6 +9,13 @@ import { PetsModule } from './pets/pets.module';
 
 @Module({
   imports: [
+
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database:  ':memory:',
+      entities: ['/dist/**/*/*.entity{.ts,.js}'],
+      synchronize: true
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
