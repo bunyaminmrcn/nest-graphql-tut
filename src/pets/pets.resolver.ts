24d639f6566ed5330d28d40a1520788225da0eb9
bunyaminmrcn <bunyaminmrcn@gmail.com>
2022-10-19 21:06:03 +0300

    GraphQL Run

--- src/pets/pets.resolver.ts
+++ src/pets/pets.resolver.ts
@@ -1,4 +1,17 @@
-import { Resolver } from '@nestjs/graphql';
+import { Query, Resolver } from '@nestjs/graphql';
+import { Pet } from './pet.entity';
+import { PetsService } from './pets.service';
 
-@Resolver()
-export class PetsResolver {}
+@Resolver(of => Pet)
+export class PetsResolver {
+
+    constructor(private petService: PetsService) {
+
+    }
+
+    @Query(returns => [Pet])
+    pets(): Promise<Pet[]> {
+        return this.petService.findAll();
+
+    }
+}
